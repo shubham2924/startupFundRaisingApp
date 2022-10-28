b708bb15eccdbd27f7081caf7ba6b69302e89491
shubham2924 <metkar9699130773@gmail.com>
2022-10-28 11:34:27 +0530

    update: image upload feature added from storage to firestore

--- mock project-frontend/src/components/Registration.js
+++ mock project-frontend/src/components/Registration.js
@@ -12,18 +12,19 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { MenuItem, Select } from "@mui/material";
-import { InputLabel } from "@mui/material";
 import { FormControl } from "@mui/material";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Navbar from "./Navbar";
 import { doc, updateDoc } from "firebase/firestore";
-import { auth,app } from "../firebase-config";
+import { auth, app } from "../firebase-config";
 import { getFirestore } from "firebase/firestore";
-import {useRef} from 'react';
+import { useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
-const db=getFirestore(app)
+import { storage } from "../firebase-config";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+const db = getFirestore(app);
 const domains = [
   {
     value: "Educational",
@@ -55,18 +56,41 @@ export default function Registration() {
   const compDesc = useRef(null);
   const [companyName, setCompanyName] = useState("");
   const [companyEmail, setCompanyEmail] = useState("");
+  const [imageURL, setImageURL] = useState("");
+  const useremailid = localStorage.getItem("useremail");
+  const collectionRef = doc(db, "users", useremailid);
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
-    const useremailid = localStorage.getItem("useremail");
-    const collectionRef = doc(db, "users", useremailid);
-    await updateDoc(collectionRef, {
-      "companyDesc": desc,
-      "companyName": companyName,
-      "companyEmail": companyEmail,
-      "companyDomain": domain,
-      "DOI": value.$d,
-    });
+    const file = event.target[0].files[0];
+    if (!file) return;
+    const storageRef = ref(storage, `images/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // const progress =
+        //   Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+        // setProgresspercent(progress);
+        console.log(snapshot);
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+          // setImageURL(downloadURL);
+          await updateDoc(collectionRef, {
+            companyDesc: desc,
+            companyName: companyName,
+            companyEmail: companyEmail,
+            companyDomain: domain,
+            DOI: value.$d,
+            imageurl: downloadURL,
+          });
+        });
+      }
+    );
+
     console.log(desc);
     console.log(companyName);
     console.log(value.$d);
@@ -74,7 +98,7 @@ export default function Registration() {
     console.log(domain);
     event.target.reset();
     navigate("/home");
-    localStorage.setItem("registered",true);
+    localStorage.setItem("registered", true);
   };
   const [domain, setDomain] = React.useState("");
 
@@ -83,151 +107,160 @@ export default function Registration() {
   };
   const [value, setValue] = useState(null);
 
-  const [images, setImages] = useState([]);
-  const [imageURLs, setImageURLs] = useState([]);
-
-  useEffect(() => {
-    if (images.length > 1) return;
-    const newImageURLs = [];
-    images.forEach((image) => newImageURLs.push(URL.createObjectURL(image)));
-    setImageURLs(newImageURLs);
-  }, [images]);
-
-  function onImageChange(event) {
-    setImages([event.target.files]);
-  }
-  const isregistered = localStorage.getItem("registered")
+  // const [images, setImages] = useState([]);
+  //.......currently not in use function, kept for future reference........//
+  // function onImageChange(event) {
+  //   setImages([event.target.files]);
+  // }
+  const isregistered = localStorage.getItem("registered");
   return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Navbar />
-        {isregistered?<h2>Already registered</h2>:
-        <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Registration
-        </Typography>
-        <Box
-          component="form"
-          noValidate
-          onSubmit={handleSubmit}
-          sx={{ mt: 3 }}
-        >
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-              ref={compName}
-                autoComplete="given-name"
-                name="companyName"
-                required
-                fullWidth
-                id="companyName"
-                label="Company Name"
-                autoFocus
-                onChange={(event) => {
-                  setCompanyName(event.target.value);
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <Box sx={{ minWidth: 120 }}>
-                <FormControl fullWidth>
-                  <TextField
-                  ref={compDomain}
-                    id="domain"
-                    select
-                    required
-                    label="Company Domain"
-                    value={domain}
-                    onChange={handleChange}
-                    // helperText="Please select your Domain"
-                  >
-                    {domains.map((option) => (
-                      <MenuItem key={option.value} value={option.value}>
-                        {option.label}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                </FormControl>
-              </Box>
-            </Grid>
-
-            <Grid item xs={12}>
-              <TextField
-              ref={compEmail}
-                required
-                fullWidth
-                id="email"
-                label=" Company Email Address"
-                name="email"
-                autoComplete="email"
-                onChange={(event) => {
-                  setCompanyEmail(event.target.value);
-                }}
-              />
-            </Grid>
-
-            <Grid item xs={8}>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                
-                  autoFocus
-                  label="Date of Establishment"
-                  value={value}
-                  onChange={(newValue) => {
-                    setValue(newValue);
-                  }}
-                  renderInput={(params) => <TextField ref={compDOI} {...params} />}
-                />
-              </LocalizationProvider>
-            </Grid>
-
-            <Grid item xs={12}>
-              <TextField
-              ref={compDesc}
-                required
-                fullWidth
-                id="desc"
-                label=" Company Description"
-                name="desc"
-                autoComplete="desc"
-                autoFocus
-                onChange={(event) => {
-                  setDesc(event.target.value);
-                }}
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={
-                  <Checkbox value="allowExtraEmails" color="primary" />
-                }
-                label="I declare that the information provided is legit and correct."
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+        {isregistered ? (
+          <h2>Already registered</h2>
+        ) : (
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            Register
-          </Button>
-        </Box>
-      </Box>
-        }
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Registration
+            </Typography>
+            <Box
+              component="form"
+              noValidate
+              onSubmit={handleSubmit}
+              sx={{ mt: 3 }}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    required
+                    label="Company Logo"
+                    input
+                    type="file"
+                    multiple
+                    accept="image/*"
+                    // onChange={onImageChange}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    ref={compName}
+                    autoComplete="given-name"
+                    name="companyName"
+                    required
+                    fullWidth
+                    id="companyName"
+                    label="Company Name"
+                    autoFocus
+                    onChange={(event) => {
+                      setCompanyName(event.target.value);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                      <TextField
+                        ref={compDomain}
+                        id="domain"
+                        select
+                        required
+                        label="Company Domain"
+                        value={domain}
+                        onChange={handleChange}
+                        // helperText="Please select your Domain"
+                      >
+                        {domains.map((option) => (
+                          <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </FormControl>
+                  </Box>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    ref={compEmail}
+                    required
+                    fullWidth
+                    id="email"
+                    label=" Company Email Address"
+                    name="email"
+                    autoComplete="email"
+                    onChange={(event) => {
+                      setCompanyEmail(event.target.value);
+                    }}
+                  />
+                </Grid>
+
+                <Grid item xs={8}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      autoFocus
+                      label="Date of Establishment"
+                      value={value}
+                      onChange={(newValue) => {
+                        setValue(newValue);
+                      }}
+                      renderInput={(params) => (
+                        <TextField ref={compDOI} {...params} />
+                      )}
+                    />
+                  </LocalizationProvider>
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    ref={compDesc}
+                    required
+                    fullWidth
+                    id="desc"
+                    label=" Company Description"
+                    name="desc"
+                    autoComplete="desc"
+                    autoFocus
+                    onChange={(event) => {
+                      setDesc(event.target.value);
+                    }}
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="I declare that the information provided is legit and correct."
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Register
+              </Button>
+            </Box>
+          </Box>
+        )}
       </Container>
     </ThemeProvider>
   );
